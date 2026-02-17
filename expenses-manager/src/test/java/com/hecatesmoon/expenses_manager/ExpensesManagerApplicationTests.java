1f1f6a292e00fa95327de18df41aef75cf129199
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-02-17 18:18:04 -0300

    feat: initialize Spring Boot project with Maven

---
+++ expenses-manager/src/test/java/com/hecatesmoon/expenses_manager/ExpensesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hecatesmoon.expenses_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
