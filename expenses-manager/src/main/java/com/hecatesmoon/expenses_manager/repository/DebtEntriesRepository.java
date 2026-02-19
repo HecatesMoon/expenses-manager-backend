39a42ca0dcc59ae95f73c6074411bf9b93957428
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-02-19 20:31:12 -0300

    feat: create simple repo for DebtEntries

---
+++ expenses-manager/src/main/java/com/hecatesmoon/expenses_manager/repository/DebtEntriesRepository.java
@@ -0,0 +1,16 @@
+package com.hecatesmoon.expenses_manager.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hecatesmoon.expenses_manager.model.DebtEntry;
+
+@Repository
+public interface DebtEntriesRepository extends JpaRepository<DebtEntry, Long>{
+    List<DebtEntry> findAll();
+
+    DebtEntry save();
+
+}
