d8418991ccf73593d4ade9453c357af7f2dd164d
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-02-19 20:33:26 -0300

    feat: create simple service for DebtEntries

---
+++ expenses-manager/src/main/java/com/hecatesmoon/expenses_manager/service/DebtEntriesService.java
@@ -0,0 +1,25 @@
+package com.hecatesmoon.expenses_manager.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hecatesmoon.expenses_manager.model.DebtEntry;
+import com.hecatesmoon.expenses_manager.repository.DebtEntriesRepository;
+
+@Service
+public class DebtEntriesService {
+
+    @Autowired
+    private final DebtEntriesRepository debtEntriesRepository;
+
+    public DebtEntriesService(DebtEntriesRepository debtEntriesRepository){
+        this.debtEntriesRepository = debtEntriesRepository;
+    }
+
+    public List<DebtEntry> getAll(){
+        return this.debtEntriesRepository.findAll();
+    }
+    
+}
