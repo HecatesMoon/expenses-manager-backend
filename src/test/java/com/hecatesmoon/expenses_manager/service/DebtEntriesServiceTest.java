e4145138ad972f205fe4a699de29ebf56e03bf28
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-08-31 23:21:13 -0400

    test: add simple tests for getById in DebtEntriesSer vice

--- src/test/java/com/hecatesmoon/expenses_manager/service/DebtEntriesServiceTest.java
+++ src/test/java/com/hecatesmoon/expenses_manager/service/DebtEntriesServiceTest.java
@@ -4,11 +4,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.hecatesmoon.expenses_manager.dto.DebtEntryResponse;
+import com.hecatesmoon.expenses_manager.exception.ResourceNotFoundException;
 import com.hecatesmoon.expenses_manager.model.DebtEntry;
+import com.hecatesmoon.expenses_manager.model.User;
 import com.hecatesmoon.expenses_manager.repository.DebtEntriesRepository;
 import com.hecatesmoon.expenses_manager.repository.UsersRepository;
 
@@ -44,6 +48,39 @@ public class DebtEntriesServiceTest {
         List<DebtEntry> result = debtEntriesService.getAll();
 
         Assertions.assertIterableEquals(list, result);
+    }
+
+    @Test
+    public void getById_invalidId(){
+        long id = 12l;
+        long userId = 6l;
+        when(debtEntriesRepoMock.findById(id)).thenThrow(new ResourceNotFoundException("entry not found, id: " + id));
+
+        Assertions.assertThrows(ResourceNotFoundException.class, ()->{
+            debtEntriesService.getById(id, userId);
+        });
+
+        //todo: verify methods are not called
+    }
+
+    @Test
+    public void getById_validIdAndUserIsOwner(){
+        long id = 12l;
+        long userId = 4l;
+        DebtEntry entry = new DebtEntry();
+        entry.setId(id);
+        User user = new User();
+        user.setId(userId);
+        entry.setUser(user);
+
+        when(debtEntriesRepoMock.findById(id)).thenReturn(Optional.of(entry));
+
+        DebtEntryResponse expected = DebtEntryResponse.from(entry);
+        DebtEntryResponse result = debtEntriesService.getById(id, userId);
+
+        //todo: make isequal method for dto
+        Assertions.assertEquals(expected.getId(), result.getId());
+
     }
     
 }
