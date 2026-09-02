a63a1ecbd9e3b14781ef79c28ca94f8cc1070665
JoacoOrbit <88943020+HecatesMoon@users.noreply.github.com>
2026-09-01 20:56:26 -0400

    chore: clean imports

--- src/test/java/com/hecatesmoon/expenses_manager/service/DebtEntriesServiceTest.java
+++ src/test/java/com/hecatesmoon/expenses_manager/service/DebtEntriesServiceTest.java
@@ -1,7 +1,6 @@
 package com.hecatesmoon.expenses_manager.service;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
